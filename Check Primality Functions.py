2c7e7a15afdd01a4501349e163c8db9cd7a95e49
Andy Tommey <57177468+andytommey@users.noreply.github.com>
2020-02-27 13:50:56 +0000

    initial commit

---
+++ Check Primality Functions.py
@@ -0,0 +1,21 @@
+"""Ask the user for a number and determine whether the number is prime or not.
+(For those who have forgotten, a prime number is a number that has no divisors.).
+ You can (and should!) use your answer to Exercise 4 to help you. """
+
+
+def is_prime():
+    number = int(input("Enter a number: "))
+    divisors = range(1, number + 1)
+    newdivisors = []
+    for i in divisors:
+        if number % i == 0:
+            newdivisors.append(i)
+    if len(newdivisors) == 2:
+        return str(number) + " is a prime number"
+    elif number == 1:
+        return str(number) + " is a prime number"
+    else:
+        return str(number) + " is not a prime number"
+
+
+print(is_prime())
